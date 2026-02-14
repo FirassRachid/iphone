c785c92db398c54322d6655411779bfa27e7f12e
Firass Rachid <firassrachid2000@gmail.com>
2026-02-14 11:03:49 +0300

    Add files via upload

---
+++ iphone.js
@@ -0,0 +1,10 @@
+let main_photo = document.querySelector('.main_photo');
+let container = document.querySelector('.container');
+function photo(p){
+ main_photo.src  = p;
+}
+
+
+function Color(c){
+  container.style.background = c;
+}
